45f4923632a678d450ef094e35a11f7f222419ab
maffettone <pmaffetto@bnl.gov>
2025-05-14 20:54:37 +0000

    try a different route

--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -5,8 +5,8 @@ whoami
 echo "Running in $(pwd)"
 echo "Running in $(dirname $0)"
 echo "Running in $(realpath $0)"
-sudo chown -R ros:ros src
 ls -la
+sudo su root
 ./setup.sh
 ./build.sh
 # ./test.sh
